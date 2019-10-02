2b4004eba05e5d553a3133127e739353c5ad20cc
TechnoPharmer <technopharmer2000@gmail.com>
2019-10-02 17:08:03 -0400

    house hunt updates

--- py_dev/house_hunt/viability_calculator.py
+++ py_dev/house_hunt/viability_calculator.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 city_data = requests.get('https://www.zip-codes.com/county/oh-cuyahoga.asp')
-#property_data = requests.get()
+property_data = requests.get(input("Enter the property URL from Realtor.com: "))
 #tax_data = requests.get()
 
 city_zip = []
@@ -19,5 +19,10 @@ for link in links:
     zip = title.split()[2].strip(',')
     city_zip.append((city, zip))
 
-print(city_zip)
+#get property data from realtor.com
+soup = BeautifulSoup(property_data.text, "html.parser")
 
+#prop_info = soup.find('div', {'class': 'jsx-3586775753 listing-information-cmp'})
+price = soup.find('span', {'class': 'jsx-3586775753 price'}).get_text()
+
+print (price)
